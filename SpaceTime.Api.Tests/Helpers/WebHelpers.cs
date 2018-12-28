ff427a938b8cd38f4d5f2a5d215e3b58e5a99662
Bob Hall <myk_quayce@hotmail.com>
2018-12-28 09:18:25 +0000

    adding unit test for default controller

---
+++ SpaceTime.Api.Tests/Helpers/WebHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SpaceTime.Api.Tests.Helpers
+{
+	public static class WebHelpers
+	{
+		public static async Task<(HttpStatusCode, HttpHeaders, string)> GetAsync(string url)
+		{
+			var request = new HttpRequestMessage(HttpMethod.Get, url);
+			var response = await AssemblyInitialize.Client.SendAsync(request, CancellationToken.None);
+
+			var statusCode = response.StatusCode;
+			var headers = response.Headers;
+			var body = await response.Content?.ReadAsStringAsync();
+
+			return (statusCode, headers, body);
+		}
+	}
+}
