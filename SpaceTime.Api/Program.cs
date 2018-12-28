0b73ea3fd99a3fa33c7d5aa18a0b6a124651b7a9
Bob Hall <myk_quayce@hotmail.com>
2018-12-28 09:18:01 +0000

    Add project files.

--- SpaceTime.Api/Program.cs
+++ SpaceTime.Api/Program.cs
@@ -1,21 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace SpaceTime.Api
 {
 	public class Program
 	{
-		public static void Main(string[] args)
-		{
-			CreateWebHostBuilder(args).Build().Run();
-		}
+		public static Task Main(string[] args) => CreateWebHostBuilder(args).Build().RunAsync();
 
 		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
 			WebHost.CreateDefaultBuilder(args)
